82310bf777c58c513ba1b356bb60ba5b24812a38
Daniel Kofi Yamoah <dnnkofi@gmail.com>
2025-10-04 14:06:10 +0000

    wrote a script for retrieving userinputs

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     
     const feedbackDiv = document.getElementById("form-feedback");
   // All your JavaScript logic goes here
+    // retrieving user input
+  const username = usernameInput.value.input.trim()
+  const email = emailInput.value.input.trim()
+  const password = userInput.value.input.trim()
 });
+    
