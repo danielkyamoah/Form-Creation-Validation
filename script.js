2407d55a2d4e4d95b1b381582b1d730bff83c992
Daniel Kofi Yamoah <dnnkofi@gmail.com>
2025-10-04 13:49:22 +0000

    created a const and then got element by id name

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("registration-form");
+    
+    const feedbackDiv = document.getElementById("form-feedback");
   // All your JavaScript logic goes here
 });
